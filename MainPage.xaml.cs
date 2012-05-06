e9f007e6719d1c404c784bdf20559c7b1b7df345
factormystic <factormystic@gmail.com>
2012-05-06 01:20:21 -0400

    initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Devices.Sensors;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Reactive;
+
+namespace Rx_Compass_Smoothing
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        Compass compass = new Compass();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Observable.FromEvent<SensorReadingEventArgs<CompassReading>>(compass, "CurrentValueChanged")
+                .BufferWithTime(TimeSpan.FromSeconds(2))
+                .Select(headings => headings.Select(e => e.EventArgs.SensorReading.TrueHeading).Sum() / headings.Count())
+                .ObserveOnDispatcher()
+                .Subscribe(heading => this.ArrowRotation.Angle = 360 - heading);
+
+            compass.Start();
+        }
+    }
+}
